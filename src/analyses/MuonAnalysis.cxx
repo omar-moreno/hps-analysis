862fa4e301b90ffff5a1cfdc1ab87b809b9e54b9
Omar Moreno <email@omarmoreno.net>
2015-08-04 14:16:57 -0700

    Match 'muon' clusters to tracks and make plots of the clusters that have been matched.

--- src/analyses/MuonAnalysis.cxx
+++ src/analyses/MuonAnalysis.cxx
@@ -86,6 +86,34 @@ void MuonAnalysis::processEvent(HpsEvent* event) {
 
     plotter->get1DHistogram("cluster energy sum - cuts: size, fiducial")->Fill(pair[0]->getEnergy() + pair[1]->getEnergy());
 
+    SvtTrack* first_track = NULL;
+    SvtTrack* second_track = NULL; 
+    for (int track_n = 0; track_n < event->getNumberOfTracks(); ++track_n) { 
+        if (AnalysisUtils::isMatch(pair[0], event->getTrack(track_n))) { 
+            first_track = event->getTrack(track_n);
+        } else if (AnalysisUtils::isMatch(pair[1], event->getTrack(track_n))) { 
+            second_track = event->getTrack(track_n);
+        }
+    }
+
+    if (first_track == NULL && second_track == NULL) {
+        return;
+    } else if (first_track == NULL || second_track == NULL) { 
+        return;
+    }
+    
+    plotter->get2DHistogram("cluster pair energy - matched")->Fill(pair[0]->getEnergy(), pair[1]->getEnergy());
+    plotter->get1DHistogram("cluster pair time dt - matched")->Fill(pair[0]->getClusterTime() - pair[1]->getClusterTime());
+    plotter->get2DHistogram("cluster pair time - matched")->Fill(pair[0]->getClusterTime(), pair[1]->getClusterTime()); 
+    plotter->get2DHistogram("cluster x position - matched")->Fill(pair[0]->getPosition()[0], 
+            pair[1]->getPosition()[0]);
+    plotter->get2DHistogram("cluster y position - matched")->Fill(pair[0]->getPosition()[1], 
+            pair[1]->getPosition()[1]);
+    plotter->get2DHistogram("cluster y v cluster x - matched")->Fill(pair[1]->getPosition()[0], 
+            pair[1]->getPosition()[1]);
+
+    plotter->get1DHistogram("cluster energy sum - matched")->Fill(pair[0]->getEnergy() + pair[1]->getEnergy());
+
 }
 
 void MuonAnalysis::finalize() {
@@ -113,9 +141,14 @@ void MuonAnalysis::bookHistograms() {
     plotter->get2DHistogram("cluster pair energy - cuts: size, fiducial")->GetXaxis()->SetTitle("Cluster energy (GeV)");
     plotter->get2DHistogram("cluster pair energy - cuts: size, fiducial")->GetYaxis()->SetTitle("Cluster energy (GeV)");
 
+    plotter->build2DHistogram("cluster pair energy - matched", 50, 0, 1.5, 50, 0, 1.5);
+    plotter->get2DHistogram("cluster pair energy - matched")->GetXaxis()->SetTitle("Cluster energy (GeV)");
+    plotter->get2DHistogram("cluster pair energy - matched")->GetYaxis()->SetTitle("Cluster energy (GeV)");
+
     plotter->build1DHistogram("cluster energy sum", 50, 0, 1.5)->GetXaxis()->SetTitle("Cluster energy sum (GeV)");
     plotter->build1DHistogram("cluster energy sum - cuts: size", 50, 0, 1.5)->GetXaxis()->SetTitle("Cluster energy sum (GeV)");
     plotter->build1DHistogram("cluster energy sum - cuts: size, fiducial", 50, 0, 1.5)->GetXaxis()->SetTitle("Cluster energy sum (GeV)");
+    plotter->build1DHistogram("cluster energy sum - matched", 50, 0, 1.5)->GetXaxis()->SetTitle("Cluster energy sum (GeV)");
 
     // Cluster time //
 
@@ -137,6 +170,12 @@ void MuonAnalysis::bookHistograms() {
     plotter->get2DHistogram("cluster pair time - cuts: size, fiducial")->GetXaxis()->SetTitle("Cluster time (ns)");
     plotter->get2DHistogram("cluster pair time - cuts: size, fiducial")->GetYaxis()->SetTitle("Cluster time (ns)");
 
+    plotter->build1DHistogram("cluster pair time dt - matched", 40, -10, 10);
+
+    plotter->build2DHistogram("cluster pair time - matched", 250, 0, 125, 250, 0, 125);
+    plotter->get2DHistogram("cluster pair time - matched")->GetXaxis()->SetTitle("Cluster time (ns)");
+    plotter->get2DHistogram("cluster pair time - matched")->GetYaxis()->SetTitle("Cluster time (ns)");
+    
     //-------------------------//
     //--- Cluster positions ---//
     //-------------------------//
@@ -173,6 +212,18 @@ void MuonAnalysis::bookHistograms() {
     plotter->get2DHistogram("cluster y position - cuts: size, fiducial")->GetYaxis()->SetTitle("Cluster position - y (mm)");
 
     plotter->build2DHistogram("cluster y v cluster x - cuts: size, fiducial", 200, -200, 200, 100, -100, 100);
+
+    plotter->build2DHistogram("cluster x position - matched", 200, -200, 200, 200, -200, 200);
+    plotter->get2DHistogram("cluster x position - matched")->GetYaxis()->SetTitle("Cluster position - x (mm)");
+    plotter->get2DHistogram("cluster x position - matched")->GetYaxis()->SetTitle("Cluster position - x (mm)");
+
+    plotter->build2DHistogram("cluster y position - matched", 200, -200, 200, 200, -200, 200);
+    plotter->get2DHistogram("cluster y position - matched")->GetYaxis()->SetTitle("Cluster position - y (mm)");
+    plotter->get2DHistogram("cluster y position - matched")->GetYaxis()->SetTitle("Cluster position - y (mm)");
+
+    plotter->build2DHistogram("cluster y v cluster x - matched", 200, -200, 200, 100, -100, 100);
+
+
 } 
 
 std::string MuonAnalysis::toString() { 
