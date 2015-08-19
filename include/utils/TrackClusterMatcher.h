094bfb10560ccb89ca9604b0dc391a63f5f7a807
Omar Moreno <email@omarmoreno.net>
2015-08-19 10:25:26 -0700

    Tighten up the track-cluster delta x/y cuts.  Add documentation.

--- include/utils/TrackClusterMatcher.h
+++ include/utils/TrackClusterMatcher.h
@@ -1,11 +1,29 @@
+/**
+ * @file TrackClusterMatcher.h
+ * @brief  A class used to find all Ecal cluster - track matches
+ * @author Omar Moreno <omoreno1@ucsc.edu> \n
+ *         Santa Cruz Institute for Particle Physics
+ *         University of California, Santa Cruz
+ * @date June 16, 2015 
+ */
+
 
 #ifndef __TRACK_CLUSTER_MATCHER_H__
 #define __TRACK_CLUSTER_MATCHER_H__
 
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
 #include <map>
 
+//--------------------//
+//--- HPS Analysis ---//
+//--------------------//
 #include <TrackExtrapolator.h>
 
+//-----------------//
+//--- HPS Event ---//
+//-----------------//
 #include <HpsEvent.h>
 #include <SvtTrack.h>
 #include <EcalCluster.h>
@@ -15,36 +33,64 @@ class TrackClusterMatcher {
     public: 
 
         /**
-         * Constructor
+         * Constructor.
          */
         TrackClusterMatcher();
 
         /**
-         *
+         * Destructor.
          */
         ~TrackClusterMatcher();
 
         /**
+         * Find all cluster and track matches in the event.
          *
+         * @param event : HpsEvent containing the tracks and clusters of 
+         *                interest
          */
         void findAllMatches(HpsEvent* event);
 
         /**
+         * Get a track that is matched to the given cluster.
          *
+         * @param cluster : The Ecal cluster of interest 
          */
-        SvtTrack* getMatchingTrack(EcalCluster* cluster);
+        SvtTrack* getMatchingTrack(EcalCluster* cluster) { return cluster_map[cluster]; };
 
         /**
+         * Get a cluster that is matched to the given track.
          *
+         * @param track : The SVT track
          */
-        EcalCluster* getMatchingCluster(SvtTrack* track);
+        EcalCluster* getMatchingCluster(SvtTrack* track) { return track_map[track]; };
 
     private:
 
+        /**
+         * Check if an Ecal cluster and SVT track are a track match.
+         *
+         * @param track : The SVT track
+         * @param cluster : The Ecal cluster of interest 
+         */
         bool isMatch(EcalCluster* cluster, SvtTrack* track);
 
+        /** Map from an Ecal cluster to a track matched to it */
         std::map <EcalCluster*, SvtTrack*> cluster_map;
+
+        /** Map from an SVT track to a cluster matched to it */
         std::map <SvtTrack*, EcalCluster*> track_map;
+
+        /** Plotter */
+
+        double top_cluster_track_match_delta_x_low; 
+        double bottom_cluster_track_match_delta_x_low; 
+        double top_cluster_track_match_delta_x_high; 
+        double bottom_cluster_track_match_delta_x_high; 
+        
+        double top_cluster_track_match_delta_y_low; 
+        double bottom_cluster_track_match_delta_y_low; 
+        double top_cluster_track_match_delta_y_high; 
+        double bottom_cluster_track_match_delta_y_high; 
 };
 
 #endif // __TRACK_CLUSTER_MATCHER_H__
--- src/utils/TrackClusterMatcher.cxx
+++ src/utils/TrackClusterMatcher.cxx
@@ -1,26 +1,53 @@
+/**
+ * @file TrackClusterMatcher.h
+ * @brief  A class used to find all Ecal cluster - track matches
+ * @author Omar Moreno <omoreno1@ucsc.edu> \n
+ *         Santa Cruz Institute for Particle Physics
+ *         University of California, Santa Cruz
+ * @date June 16, 2015 
+ */
 
 #include <TrackClusterMatcher.h>
 
-TrackClusterMatcher::TrackClusterMatcher() {
+TrackClusterMatcher::TrackClusterMatcher() 
+    : top_cluster_track_match_delta_x_low(-7.61),
+      bottom_cluster_track_match_delta_x_low(-13.75),
+      top_cluster_track_match_delta_x_high(12),
+      bottom_cluster_track_match_delta_x_high(6),
+      top_cluster_track_match_delta_y_low(-14),
+      bottom_cluster_track_match_delta_y_low(-14),
+      top_cluster_track_match_delta_y_high(14),
+      bottom_cluster_track_match_delta_y_high(14)
+{
 }
 
 TrackClusterMatcher::~TrackClusterMatcher() { 
+    cluster_map.clear();
+    track_map.clear(); 
 }
 
 void TrackClusterMatcher::findAllMatches(HpsEvent* event) {
 
+    // Clear the track and cluster maps of all previously found track and 
+    // cluster matches.
     cluster_map.clear();
-    track_map.clear();    
+    track_map.clear();
+
+    // Loop over all of the clusters in the event and try to find a track
+    // match for it.
     for (int cluster_n = 0; cluster_n < event->getNumberOfEcalClusters(); ++cluster_n) {
     
         // Get the cluster from the event 
         EcalCluster* cluster = event->getEcalCluster(cluster_n);
     
+        // Loop over all of the tracks in the event and try to find a match to
+        // a cluster
         for (int track_n = 0; track_n < event->getNumberOfTracks(); ++track_n) { 
         
             // Get a track from the event
             SvtTrack* track = event->getTrack(track_n);
-             
+            
+            // Check if the track and cluster match 
             if (this->isMatch(cluster, track)) {
                 cluster_map[cluster] = track;
                 track_map[track] = cluster;
@@ -30,35 +57,36 @@ void TrackClusterMatcher::findAllMatches(HpsEvent* event) {
     } 
 }
 
-
-SvtTrack* TrackClusterMatcher::getMatchingTrack(EcalCluster* cluster) { 
-    return cluster_map[cluster]; 
-}
-
-EcalCluster* TrackClusterMatcher::getMatchingCluster(SvtTrack* track) { 
-    return track_map[track];
-}
-
 bool TrackClusterMatcher::isMatch(EcalCluster* cluster, SvtTrack* track) { 
+    
+    // Check that the track and cluster are in the same detector volume.
+    // If not, thre is no way they can match.
+    if (track->isTopTrack() && cluster->getPosition()[1] < 0
+            || track->isBottomTrack() && cluster->getPosition()[1] > 0) return false;
 
     // Get the cluster position
     std::vector<double> cluster_pos = cluster->getPosition();
-
+    
     // Extrapolate the track to the Ecal cluster position 
-    std::vector<double> track_pos_at_cluster_shower_max 
+    std::vector<double> track_pos_at_ecal 
         = TrackExtrapolator::extrapolateTrack(track, cluster_pos[2]);
+    
+    double p = AnalysisUtils::getMagnitude(track->getMomentum());
 
-    // If the track and cluster are in opposite volumes, then they can't 
-    // be a match
-    if (cluster_pos[1]*track_pos_at_cluster_shower_max[1] < 0) return false;
+    double delta_x = cluster_pos[0] - track_pos_at_ecal[0];
+    double delta_y = cluster_pos[1] - track_pos_at_ecal[1];
 
     // Check that dx and dy between the extrapolated track and cluster
     // positions is reasonable
-    if (cluster_pos[0] - track_pos_at_cluster_shower_max[0] > 12 ||
-            cluster_pos[0] - track_pos_at_cluster_shower_max[0] < -18) return false;
+    if ((track->isTopTrack() && (delta_x > top_cluster_track_match_delta_x_high ||
+                    delta_x < top_cluster_track_match_delta_x_low)) ||
+        (track->isBottomTrack() && (delta_x > bottom_cluster_track_match_delta_x_high ||
+                                   delta_x < bottom_cluster_track_match_delta_x_low ))) return false;
     
-    if (cluster_pos[1] - track_pos_at_cluster_shower_max[1] > 12
-            || cluster_pos[1] - track_pos_at_cluster_shower_max[1] < -18) return false;
+    if ((track->isTopTrack() && (delta_y > top_cluster_track_match_delta_y_high ||
+                    delta_y < top_cluster_track_match_delta_y_low)) ||
+        (track->isBottomTrack() && (delta_y > bottom_cluster_track_match_delta_y_high ||
+                                    delta_y < bottom_cluster_track_match_delta_y_low))) return false;
 
     return true;
 }
