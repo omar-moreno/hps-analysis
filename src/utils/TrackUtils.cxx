9d47e7ddd7c48e8f24deeb33ed166d86c385265d
Omar Moreno <omoreno1@ucsc.edu>
2015-09-16 14:56:03 -0700

    When looking at duplicate tracks i.e. tracks containing the same hits, keep the track with the best chi2.

--- src/utils/TrackUtils.cxx
+++ src/utils/TrackUtils.cxx
@@ -12,46 +12,67 @@
 #include <TrackUtils.h>
 
 std::vector<SvtTrack*> TrackUtils::getGoodTracksList(HpsEvent* event) { 
-    
-    std::cout << "Event: " << event->getEventNumber() << std::endl;
-    std::cout << "Get number of tracks: " << event->getNumberOfTracks() << std::endl;
 
-    std::vector<SvtTrack*> tracks; 
-    std::map<std::vector<SvtHit*>, std::vector<SvtTrack*>> equivalent_tracks;
+    //std::cout << "Event: " << event->getEventNumber() << std::endl;
+    //std::cout << "Get number of tracks: " << event->getNumberOfTracks() << std::endl;
 
+    // Map the hits on a track to the corresponding track
+    std::map<std::vector<SvtHit*>, std::vector<SvtTrack*>> hit_to_tracks_map;
+
+    std::vector<SvtTrack*> tracks;
+
+    // Loop over all of the tracks and remove GBL tracks and duplicates
     for (int track_n = 0; track_n < event->getNumberOfTracks(); ++track_n) { 
-        
+
+        // Get a track from the event 
         SvtTrack* track = event->getTrack(track_n);
-        
+
+        // Only add seed tracks to the list of 'good' tracks
         if (TrackType::isGbl(track)) { 
-            std::cout << "GBL track found! Skipping it" << std::endl;
+            //std::cout << "GBL track found! Skipping it" << std::endl;
             continue; 
         }
-        
+    
+        // Get the hits on a track
         TRefArray* track_hits = track->getSvtHits(); 
-        std::vector<SvtHit*> hits(6, nullptr);  
+
+        // Add the hits to a std::vector.  This is done to take advantage of 
+        // a std::vector's == operator.
+        std::vector<SvtHit*> hit_vector(6, nullptr);  
         for (int hit_n = 0; hit_n < track_hits->GetSize(); ++hit_n) { 
             SvtHit* hit = (SvtHit*) track_hits->At(hit_n);
-            hits.at(hit->getLayer()-1) = hit;   
+            hit_vector.at(hit->getLayer()-1) = hit;   
         }
 
-        auto it = equivalent_tracks.find(hits); 
-        if (it == equivalent_tracks.end()) { 
-            std::cout << "Adding hits to map: " << std::endl;
+        // Add the list of hits to the map if it doesn't exist already
+        auto it = hit_to_tracks_map.find(hit_vector); 
+        if (it == hit_to_tracks_map.end()) { 
+            //std::cout << "Adding hits to map: " << std::endl;
             std::vector<SvtTrack*> track_list; 
-            equivalent_tracks.emplace(hits, track_list);  
+            hit_to_tracks_map.emplace(hit_vector, track_list);  
         }
 
-        equivalent_tracks[hits].push_back(track);
-        std::cout << "Track size: " << equivalent_tracks[hits].size() << std::endl; 
+        // Add the track to the map
+        hit_to_tracks_map[hit_vector].push_back(track);
+        //std::cout << "Track size: " << hit_to_tracks_map[hit_vector].size() << std::endl; 
     }
 
-    std::cout << "Comparing equivalent tracks: " << std::endl;
-    for (auto& map_item : equivalent_tracks) { 
+    // Keep the best track of the duplicates. For now, best is defined as the 
+    // track with the best chi2. 
+    //std::cout << "Comparing equivalent tracks: " << std::endl;
+    for (auto& map_item : hit_to_tracks_map) { 
         int index = 0; 
-        for (auto& equivalent_track : map_item.second) { 
-            std::cout << "Track: " << index << " Chi2 " << equivalent_track->getChi2() << std::endl;
+        double chi2 = 1000;
+        SvtTrack* best_track = nullptr;  
+        for (auto& duplicate_track : map_item.second) { 
+            if (duplicate_track->getChi2() < chi2) { 
+                chi2 = duplicate_track->getChi2(); 
+                best_track = duplicate_track; 
+            }
+            //std::cout << "Track: " << index << " Chi2 " << duplicate_track->getChi2() << std::endl;
         }
+        //std::cout << "Chi2 of best track: " << best_track->getChi2() << std::endl;
+        tracks.push_back(best_track); 
     } 
 
     return tracks; 
