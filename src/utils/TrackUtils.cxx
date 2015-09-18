3625000ed46d3bcc2548cfa08ff67ab55215d9c6
Omar Moreno <omoreno1@ucsc.edu>
2015-09-17 18:17:48 -0700

    Clean up.

--- src/utils/TrackUtils.cxx
+++ src/utils/TrackUtils.cxx
@@ -19,8 +19,6 @@ std::vector<SvtTrack*> TrackUtils::getGoodTracksList(HpsEvent* event) {
     // Map the hits on a track to the corresponding track
     std::map<std::vector<SvtHit*>, std::vector<SvtTrack*>> hit_to_tracks_map;
 
-    std::vector<SvtTrack*> tracks;
-
     // Loop over all of the tracks and remove GBL tracks and duplicates
     for (int track_n = 0; track_n < event->getNumberOfTracks(); ++track_n) { 
 
@@ -28,16 +26,14 @@ std::vector<SvtTrack*> TrackUtils::getGoodTracksList(HpsEvent* event) {
         SvtTrack* track = event->getTrack(track_n);
 
         // Only add seed tracks to the list of 'good' tracks
-        if (TrackType::isGbl(track)) { 
-            //std::cout << "GBL track found! Skipping it" << std::endl;
-            continue; 
-        }
+        if (TrackType::isGbl(track)) continue; 
     
-        // Get the hits on a track
+        // Get the hits associated with a track
         TRefArray* track_hits = track->getSvtHits(); 
 
         // Add the hits to a std::vector.  This is done to take advantage of 
-        // a std::vector's == operator.
+        // a std::vector's == operator. A TRefArray doesn't have a define
+        // way to compare references to objects
         std::vector<SvtHit*> hit_vector(6, nullptr);  
         for (int hit_n = 0; hit_n < track_hits->GetSize(); ++hit_n) { 
             SvtHit* hit = (SvtHit*) track_hits->At(hit_n);
@@ -57,23 +53,35 @@ std::vector<SvtTrack*> TrackUtils::getGoodTracksList(HpsEvent* event) {
         //std::cout << "Track size: " << hit_to_tracks_map[hit_vector].size() << std::endl; 
     }
 
-    // Keep the best track of the duplicates. For now, best is defined as the 
-    // track with the best chi2. 
+    // Container to hold the 'good' tracks
+    std::vector<SvtTrack*> tracks;
+
+    // Loop through the map of hits to tracks and keep the best track of those
+    // which are duplicates i.e. they share the same hits. For now, best is
+    // defined as the track with the best chi2. 
     //std::cout << "Comparing equivalent tracks: " << std::endl;
     for (auto& map_item : hit_to_tracks_map) { 
-        int index = 0; 
-        double chi2 = 1000;
-        SvtTrack* best_track = nullptr;  
-        for (auto& duplicate_track : map_item.second) { 
-            if (duplicate_track->getChi2() < chi2) { 
-                chi2 = duplicate_track->getChi2(); 
-                best_track = duplicate_track; 
-            }
-            //std::cout << "Track: " << index << " Chi2 " << duplicate_track->getChi2() << std::endl;
+       
+        if (map_item.second.size() == 1) { 
+            tracks.push_back(map_item.second[0]); 
+            continue; 
         }
-        //std::cout << "Chi2 of best track: " << best_track->getChi2() << std::endl;
+
+        SvtTrack* best_track = map_item.second[0];  
+        for (auto& track : map_item.second) { 
+            if (track->getChi2() < best_track->getChi2()) { 
+                best_track = track; 
+            }
+        }
         tracks.push_back(best_track); 
     } 
+
+    // Loop through all of the remaining 'good' tracks and remove 
+    // 'partial tracks' i.e. tracks whose hit content is a subset of another
+    // track.
+    for (auto& track : tracks) { 
+        
+    }
 
     return tracks; 
 }
