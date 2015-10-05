7483e8059fd073be65ca2427ecda447f0afda733
Omar Moreno <email@omarmoreno.net>
2015-10-05 10:27:56 -0700

    Use a yaml config file to select which files and histograms to compare.

--- src/utils/compare_plots.cxx
+++ src/utils/compare_plots.cxx
@@ -15,6 +15,11 @@
 #include <list>
 #include <fstream>
 
+//--------------//
+//   yaml-cpp   //
+//--------------//
+#include <yaml-cpp/yaml.h>
+
 //--------------------//
 //--- HPS Analysis ---//
 //--------------------//
@@ -25,16 +30,14 @@ using namespace std;
 int main(int argc, char **argv) {
    
     
-    string root_file_list_name;
-    string histogram_name; 
+    string config_file_name; 
     string style = "";
     bool save_to_pdf = false;
 
     // Parse all the command line arguments. If there are no valid command line
     // arguments passed, print the usage and exit.
     static struct option long_options[] = {
-        {"root_list", required_argument, 0, 'l' },
-        {"name", required_argument, 0, 'n' },
+        {"config_file", required_argument, 0, 'c' }, 
         {"style", required_argument, 0, 's'},
         {"pdf", no_argument, 0, 'p' },
         { 0, 0, 0, 0 }
@@ -42,10 +45,10 @@ int main(int argc, char **argv) {
 
     int option_index = 0;
     int option_char; 
-    while ((option_char = getopt_long(argc, argv, "l:s:pn:", long_options, &option_index)) != -1) {
+    while ((option_char = getopt_long(argc, argv, "c:s:p", long_options, &option_index)) != -1) {
         switch (option_char) {
-            case 'l': 
-                root_file_list_name = optarg;
+            case 'c': 
+                config_file_name = optarg; 
                 break;
             case 's':
                 style = optarg; 
@@ -53,42 +56,46 @@ int main(int argc, char **argv) {
             case 'p':
                 save_to_pdf = true;
                 break;
-            case 'n': 
-                histogram_name = optarg;
-                break; 
             default: 
                 break;
         }
     }
 
-    if (root_file_list_name.empty()) { 
-        cerr << "Please specify a list of ROOT files to process." << endl;
-        return EXIT_FAILURE;
+    // Require a configuration
+    if (config_file_name.empty()) { 
+        cerr << "Please specify a configuration file." << endl;
+        return EXIT_FAILURE; 
     }
 
-    // Create the list of files to process
-    list< TFile* > root_files;
-    string root_file; 
-    ifstream root_file_list(root_file_list_name.c_str(), ifstream::in);
-    if (!root_file_list.is_open()) { 
-        cerr << "Failed to open file " << root_file_list_name << endl;
+    // TODO: This all needs to move to it's own config parse method
+    YAML::Node config = YAML::LoadFile(config_file_name);
+    if (config.IsNull()) { 
+        cerr << "Unable to open configuration file " << config_file_name << std::endl;
         return EXIT_FAILURE;
-    }
-
-    while (root_file_list >> root_file) { 
-        root_files.push_back(new TFile(root_file.c_str())); 
-        if (root_files.back()->IsZombie()) { 
-            cout << "Failed to open root file " << root_file << endl;
-            return EXIT_FAILURE;
-        }
     } 
-    root_file_list.close();
+    
+    YAML::Node root_file_names = config["Files"];
+    if (root_file_names.IsNull()) { 
+        cerr << "Please specify a list of ROOT files to process." << endl;
+        return EXIT_FAILURE;    
+    }
+
+    list< TFile* > root_files;
+    for (auto root_file_name : root_file_names) { 
+        root_files.push_back(new TFile(root_file_name.as<string>().c_str())); 
+        if (root_files.back()->IsZombie()) { 
+            cout << "Failed to open ROOT file " << root_file_name << std::endl;
+            return EXIT_FAILURE; 
+        } 
+    }
 
     ComparePlots* comparator = new ComparePlots();
     if (!style.empty()) comparator->setStyle(style);
 
-    if (!histogram_name.empty()) comparator->parseFiles(root_files, histogram_name);
-    else comparator->parseFiles(root_files);
+    YAML::Node histogram_names = config["Histograms"];
+    if (!histogram_names.IsNull()) { 
+        comparator->parseFiles(root_files, histogram_names[0].as<string>().c_str());
+    } else comparator->parseFiles(root_files);
     comparator->overlayPlots(); 
 
     return EXIT_SUCCESS;
