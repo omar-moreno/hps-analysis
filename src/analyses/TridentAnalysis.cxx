6e0babb29f29934e33128a422d912bae1170a6ee
Omar Moreno <omoreno1@ucsc.edu>
2015-10-05 16:35:42 -0700

    Clean up.

--- src/analyses/TridentAnalysis.cxx
+++ src/analyses/TridentAnalysis.cxx
@@ -47,30 +47,38 @@ void TridentAnalysis::processEvent(HpsEvent* event) {
     // Find all track-cluster matches in the event
     matcher->findAllMatches(event);
 
-    // Check if matches was found for the two clusters
+    // Check if matches for the two clusters were found
     if (matcher->getMatchingTrack(pair[0]) == nullptr 
             || matcher->getMatchingTrack(pair[1]) == nullptr) return;
 
-    plotter->get2DHistogram("cluster pair energy - matched")->Fill(pair[0]->getEnergy(), 
-            pair[1]->getEnergy()); 
-    plotter->get1DHistogram("cluster pair energy sum - matched")->Fill(
-            pair[0]->getEnergy() + pair[1]->getEnergy()); 
-    plotter->get1DHistogram("cluster pair energy diff - matched")->Fill(
-            pair[0]->getEnergy() - pair[1]->getEnergy());
+    // Define some commonly used variables
+    std::vector<double> cluster_energy = { 
+        pair[0]->getEnergy(), 
+        pair[1]->getEnergy()
+    };
+    
+    double cluster_energy_sum = cluster_energy[0] + cluster_energy[1]; 
+    double cluster_energy_diff = cluster_energy[0] - cluster_energy[1];
+
+    std::vector<double> cluster_time = { 
+        pair[0]->getClusterTime(), 
+        pair[1]->getClusterTime()
+    };
+
+    plotter->get2DHistogram("cluster pair energy - matched")->Fill(cluster_energy[0], cluster_energy[1]); 
+    plotter->get1DHistogram("cluster pair energy sum - matched")->Fill(cluster_energy_sum); 
+    plotter->get1DHistogram("cluster pair energy diff - matched")->Fill(cluster_energy_diff);
+    plotter->get1DHistogram("track time - cluster time - matched")->Fill(
+            matcher->getMatchingTrack(pair[0])->getTrackTime() - cluster_time[0]);  
+    plotter->get1DHistogram("track time - cluster time - matched")->Fill(
+            matcher->getMatchingTrack(pair[1])->getTrackTime() - cluster_time[1]);  
+    plotter->get1DHistogram("track pair dt - matched")->Fill(
+            matcher->getMatchingTrack(pair[0])->getTrackTime() - matcher->getMatchingTrack(pair[1])->getTrackTime()); 
 
     SvtTrack* electron = nullptr;
     SvtTrack* positron = nullptr;
 
-    if (matcher->getMatchingTrack(pair[0])->getCharge()
-            *matcher->getMatchingTrack(pair[1])->getCharge() >= 0) return; 
-
-    plotter->get2DHistogram("cluster pair energy - matched, e+e-")->Fill(pair[0]->getEnergy(), 
-            pair[1]->getEnergy()); 
-    plotter->get1DHistogram("cluster pair energy sum - matched, e+e-")->Fill(
-            pair[0]->getEnergy() + pair[1]->getEnergy()); 
-    plotter->get1DHistogram("cluster pair energy diff - matched, e+e-")->Fill(
-            pair[0]->getEnergy() - pair[1]->getEnergy());
-
+    if (matcher->getMatchingTrack(pair[0])->getCharge()*matcher->getMatchingTrack(pair[1])->getCharge() >= 0) return; 
 
     if (matcher->getMatchingTrack(pair[0])->getCharge() == -1) { 
         electron = matcher->getMatchingTrack(pair[0]);  
@@ -82,6 +90,16 @@ void TridentAnalysis::processEvent(HpsEvent* event) {
     
     double electron_p = AnalysisUtils::getMagnitude(electron->getMomentum());
     double positron_p = AnalysisUtils::getMagnitude(positron->getMomentum());
+
+    plotter->get2DHistogram("cluster pair energy - matched, e+e-")->Fill(cluster_energy[0], cluster_energy[1]); 
+    plotter->get1DHistogram("cluster pair energy sum - matched, e+e-")->Fill(cluster_energy_sum); 
+    plotter->get1DHistogram("cluster pair energy diff - matched, e+e-")->Fill(cluster_energy_diff);
+    plotter->get1DHistogram("track time - cluster time - matched, e+e-")->Fill(
+            matcher->getMatchingTrack(pair[0])->getTrackTime() - cluster_time[0]);  
+    plotter->get1DHistogram("track time - cluster time - matched, e+e-")->Fill(
+            matcher->getMatchingTrack(pair[1])->getTrackTime() - cluster_time[1]);  
+    plotter->get1DHistogram("track pair dt - matched, e+e-")->Fill(
+            matcher->getMatchingTrack(pair[0])->getTrackTime() - matcher->getMatchingTrack(pair[1])->getTrackTime()); 
 
     plotter->get2DHistogram("p[e+] v p[e-] - matched, e+e-")->Fill(electron_p, positron_p); 
      
@@ -95,25 +113,37 @@ void TridentAnalysis::finalize() {
 
 void TridentAnalysis::bookHistograms() { 
 
+    // Matched Ecal clusters
+    plotter->build1DHistogram("cluster pair energy diff - matched", 50, -1, 1)->GetXaxis()->SetTitle(
+            "Cluster Pair Energy Difference (GeV)");
+    
+    plotter->build1DHistogram("track time - cluster time - matched", 60, 30, 60)->GetXaxis()->SetTitle(
+            "Track time - Ecal cluster time"); 
+
     plotter->build2DHistogram("cluster pair energy - matched", 50, 0, 1.5, 50, 0, 1.5);
     plotter->get2DHistogram("cluster pair energy - matched")->GetXaxis()->SetTitle("Cluster Energy (GeV)");
     plotter->get2DHistogram("cluster pair energy - matched")->GetYaxis()->SetTitle("Cluster Energy (GeV)");
+
+    plotter->build1DHistogram("cluster pair energy sum - matched", 50, 0, 1.5)->GetXaxis()->SetTitle(
+            "Cluster Pair Energy Sum (GeV)");
+    
+    plotter->build1DHistogram("cluster pair energy diff - matched, e+e-", 50, -1, 1)->GetXaxis()->SetTitle(
+            "Cluster Pair Energy Difference (GeV)");
+
+    plotter->build1DHistogram("track time - cluster time - matched, e+e-", 60, 30, 60)->GetXaxis()->SetTitle(
+            "Track time - Ecal cluster time");
+
+    plotter->build1DHistogram("cluster pair energy sum - matched, e+e-", 50, 0, 1.5)->GetXaxis()->SetTitle(
+            "Cluster Pair Energy Sum (GeV)");
 
     plotter->build2DHistogram("cluster pair energy - matched, e+e-", 50, 0, 1.5, 50, 0, 1.5);
     plotter->get2DHistogram("cluster pair energy - matched, e+e-")->GetXaxis()->SetTitle("Cluster Energy (GeV)");
     plotter->get2DHistogram("cluster pair energy - matched, e+e-")->GetYaxis()->SetTitle("Cluster Energy (GeV)");
 
-    plotter->build1DHistogram("cluster pair energy sum - matched", 50, 0, 1.5)->GetXaxis()->SetTitle(
-            "Cluster Pair Energy Sum (GeV)");
-    plotter->build1DHistogram("cluster pair energy sum - matched, e+e-", 50, 0, 1.5)->GetXaxis()->SetTitle(
-            "Cluster Pair Energy Sum (GeV)");
-
-    plotter->build1DHistogram("cluster pair energy diff - matched", 50, -1, 1)->GetXaxis()->SetTitle(
-            "Cluster Pair Energy Difference (GeV)");
-    plotter->build1DHistogram("cluster pair energy diff - matched, e+e-", 50, -1, 1)->GetXaxis()->SetTitle(
-            "Cluster Pair Energy Difference (GeV)");
-
-    // Track momentum
+    // Matched tracks
+    plotter->build1DHistogram("track pair dt - matched", 50, -25, 25)->GetXaxis()->SetTitle("Track time #Delta t");  
+    plotter->build1DHistogram("track pair dt - matched, e+e-", 50, -25, 25)->GetXaxis()->SetTitle(
+            "Track time #Delta t");  
     plotter->build1DHistogram("p top - matched, e+e-", 50, 0, 1.5)->GetXaxis()->SetTitle("p (GeV)");
     plotter->build1DHistogram("p bottom - matched, e+e-", 50, 0, 1.5)->GetXaxis()->SetTitle("p (GeV)");
 
