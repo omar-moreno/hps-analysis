a0f4b884a1beb5129646b5da8bd54ca46ff4b61c
Omar Moreno <omoreno1@ucsc.edu>
2015-10-05 16:39:49 -0700

    Remove the background preprocessor method.

--- python/hps_learn/MollerPreprocessor.py
+++ python/hps_learn/MollerPreprocessor.py
@@ -13,7 +13,6 @@ class MollerPreprocessor :
         hps_dst_path += "/build/lib/libHpsEvent.so"
         root.gSystem.Load(hps_dst_path)
         
-
     def preprocess_signal(self, dst_file):
       
         tree = dst_file.Get("HPS_Event")
@@ -55,52 +54,4 @@ class MollerPreprocessor :
             
         output_file.Write()
         return output_file_name
-
-    def preprocess_background(self, dst_file): 
-        
-        tree = dst_file.Get("HPS_Event")
-
-        from ROOT import HpsEvent
-
-        hps_event = HpsEvent()
-        b_hps_event = tree.GetBranch("Event")
-        b_hps_event.SetAddress(root.AddressOf(hps_event))
-
-        output_file_name = dst_file.GetName()[str(dst_file.GetName()).rindex("/") + 1:-5]
-        output_file_name += "_preprocessed_bkg.root"
-
-        output_file = root.TFile(output_file_name, "RECREATE")
-        output_tree = root.TTree("Background", "Background Tree")
-
-        first_cluster_energy = n.zeros(1, dtype=float)
-        first_cluster_x_position = n.zeros(1, dtype=float)
-        second_cluster_energy = n.zeros(1, dtype=float)
-        second_cluster_x_position = n.zeros(1, dtype=float)
-
-        output_tree.Branch('first_cluster_energy', first_cluster_energy, 'first_cluster_energy/D')
-        output_tree.Branch('first_cluster_x_position', first_cluster_x_position, 'first_cluster_x_position/D')
-        output_tree.Branch('second_cluster_energy', second_cluster_energy, 'second_cluster_energy/D')
-        output_tree.Branch('second_cluster_x_position', second_cluster_x_position, 'second_cluster_x_position/D')
-
-        for entry in xrange(0, tree.GetEntries()):
-
-            tree.GetEntry(entry)
-
-            if not hps_event.isSingle1Trigger() : continue
-
-            if hps_event.getNumberOfEcalClusters() != 2: continue
-
-            delta_t = abs(hps_event.getEcalCluster(0).getClusterTime() - hps_event.getEcalCluster(1).getClusterTime())
-            if delta_t > 3 : continue
-
-            first_cluster_energy[0] = hps_event.getEcalCluster(0).getEnergy()
-            first_cluster_x_position[0] = hps_event.getEcalCluster(0).getPosition()[0]
-            second_cluster_energy[0] = hps_event.getEcalCluster(1).getEnergy()
-            second_cluster_x_position[0] = hps_event.getEcalCluster(1).getPosition()[0]
-           
-            output_tree.Fill()
-            
-        output_file.Write()
-        return output_file_name
-
 
