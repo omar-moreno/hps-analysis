f53d7aa9b8d8df3d8008b5bf3f04f270fb45eda9
Omar Moreno <email@omarmoreno.net>
2015-12-22 14:53:55 -0800

    Add functions to calculate the integral of the first order Chebyshev Polynomial.

--- include/fit/ChebyshevPoly.h
+++ include/fit/ChebyshevPoly.h
@@ -7,7 +7,20 @@
 
 namespace ChebyshevPoly { 
 
+    /**
+     *
+     */
     double firstOrder(double x, double par[]);
+
+    /**
+     *
+     */
+    double firstOrderIntegral(double x, double par[]);
+
+    /**
+     *
+     */
+    double firstOrderIntegral(double x_low, double x_high, double par[]); 
 }
 
 #endif // __CHEBYSHEV_POLY_H__
--- src/fit/ChebyshevPoly.cxx
+++ src/fit/ChebyshevPoly.cxx
@@ -7,3 +7,13 @@
 double ChebyshevPoly::firstOrder(double x, double par[]) {
     return par[0] + par[1] * x; 
 }
+
+
+
+double ChebyshevPoly::firstOrderIntegral(double x, double par[]) {   
+   return par[0]*x + (par[1]*x*x)/2; 
+}
+
+double ChebyshevPoly::firstOrderIntegral(double x_low, double x_high, double par[]) { 
+    return firstOrderIntegral(x_high, par) - firstOrderIntegral(x_low, par); 
+}
