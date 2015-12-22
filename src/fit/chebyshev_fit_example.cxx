5d1f0433281b5c44024067a636fd1dc224c967e6
Omar Moreno <email@omarmoreno.net>
2015-12-22 14:55:01 -0800

    Add plots of fnc and variables after every iteration.

--- src/fit/chebyshev_fit_example.cxx
+++ src/fit/chebyshev_fit_example.cxx
@@ -17,6 +17,12 @@
 /** */
 std::map<double, double> x_vec;
 
+std::vector<double> f_vals; 
+
+std::vector<double> t0;
+
+std::vector<double> t1; 
+
 /** Bin width */
 double bin_width = 0; 
 
@@ -25,6 +31,7 @@ double histo_integral = 0;
 void getData(TH1* histo) { 
     bin_width = histo->GetXaxis()->GetBinWidth(1);
     histo_integral = histo->GetEntries(); 
+    //std::cout << "Histogram integral: " << histo_integral << std::endl;
     for (int bin_n = 1; bin_n < histo->GetXaxis()->GetNbins(); ++bin_n) { 
         x_vec[histo->GetXaxis()->GetBinCenter(bin_n)] = histo->GetBinContent(bin_n); 
     }
@@ -34,22 +41,29 @@ void getData(TH1* histo) {
 void fcn(int& npar, double* deriv, double& f, double par[], int flag) {
     double lnL = 0.0; 
 
-    double total = ChebyshevPoly::firstOrder(0, par) - ChebyshevPoly::firstOrder(10, par); 
+    double norm = ChebyshevPoly::firstOrderIntegral(0, 10, par);
+    //std::cout << "Normalization: " << norm << std::endl; 
     for (auto& x : x_vec) {
         double x_low  = x.first - bin_width/2;
         double x_high = x.first + bin_width/2;
-        double func_value =  histo_integral*(ChebyshevPoly::firstOrder(x_low, par) 
-                                             - ChebyshevPoly::firstOrder(x_high, par));
+        //std::cout << "x low: "  << x_low << std::endl;
+        //std::cout << "x high: " << x_high << std::endl;
+        double func_value = histo_integral*(ChebyshevPoly::firstOrderIntegral(x_low, x_high, par)/norm); 
+        //std::cout << "v(theta) = " << func_value << std::endl; 
 
         if (func_value > 0) {
-            lnL += x.second*log(func_value) - total;
-            //lnL += log(func_value);
+            lnL += x.second*log(func_value);
         } else { 
-            std::cout << "WARNING! Value is negative." << std::endl;
+            //std::cout << "WARNING! Value is negative." << std::endl;
         }
     }
 
-    f = -lnL;
+    f = -lnL + histo_integral; 
+    
+    f_vals.push_back(f);
+    t0.push_back(par[0]); 
+    t1.push_back(par[1]); 
+    
     //std::cout << "f: " << f << std::endl; 
 }
 
@@ -67,11 +81,13 @@ int main(int argc, char** argv) {
     setupCanvas(canvas);
     
     TF1* chebyshev = new TF1("chebyshev", "[0] + [1]*x", 0, 10);
-    chebyshev->SetParameter(0, 20);
+    chebyshev->SetParameter(0, 10);
     chebyshev->SetParameter(1, -1);
 
+    std::cout << "Integral: " << chebyshev->Integral(0, 10) << std::endl;
+
     TH1F* toy_histo = new TH1F("toy_histo", "toy_histo", 100, 0, 10); 
-    toy_histo->FillRandom("chebyshev", 1000);
+    toy_histo->FillRandom("chebyshev", 100000);
     toy_histo->GetXaxis()->SetTitle("x");
     toy_histo->GetYaxis()->SetTitle("Events/100");
    
@@ -95,7 +111,7 @@ int main(int argc, char** argv) {
     double max_val[npar];
     std::string par_name[npar]; 
     
-    par[0] = 40.0; 
+    par[0] = 15.0; 
     step_size[0] = 1; 
     min_val[0] = 0;
     max_val[0] = 0; 
@@ -115,11 +131,47 @@ int main(int argc, char** argv) {
 
     minuit->Migrad();
 
+    minuit->mnhess();
+
+    //minuit->mnimpr(); 
+
+    TF1* fit_result = new TF1("fit_result", "(100000/50*((10 - x)/50))", 0, 10); 
+
     toy_histo->Draw();
     chebyshev->Draw("same");
+    fit_result->Draw("same"); 
     
-    canvas->SaveAs("chebyshev_fit_example.png"); 
-    
+    canvas->SaveAs("chebyshev_fit_example.png");
+
+    TH1F* f_histo = new TH1F("lnL", "lnL", 50, -611000, -602000); 
+    for (auto f_val : f_vals) { 
+        f_histo->Fill(f_val); 
+    }
+
+    f_histo->Draw(); 
+
+    canvas->SaveAs("lnL.png");
+
+    TH1F* t0_histo = new TH1F("t0", "t0", 50, 9.8, 10.4); 
+    for (auto t0_val : t0) { 
+        t0_histo->Fill(t0_val); 
+    }
+
+    t0_histo->Draw(); 
+
+    canvas->SaveAs("t0.png");
+
+    TH1F* t1_histo = new TH1F("t1", "t1", 50, -20, 20); 
+    for (auto t1_val : t1) { 
+        t1_histo->Fill(t1_val); 
+    }
+
+    t1_histo->Draw(); 
+
+    canvas->SaveAs("t1.png");
+
+
+
     delete toy_histo;
     delete chebyshev; 
     delete canvas; 
