e7a0ddc0bcb3d94acba9f627a28aa3b49efe215b
Omar Moreno <omoreno1@ucsc.edu>
2016-01-14 17:29:35 -0800

    Application used to evaluate the bump hunter using toy MC.

---
+++ src/fit/bump_hunt_evaluator.cxx
@@ -0,0 +1,87 @@
+/**
+ *
+ */
+
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
+#include <cstdlib>
+#include <iostream>
+#include <getopt.h>
+#include <string>
+
+#include <TFile.h>
+#include <TH1.h>
+
+#include <RootFileReader.h>
+
+using namespace std;
+
+int main(int argc, char **argv) { 
+
+    string file_name;
+    int histo_count;
+    int event_count;
+    int poly_order;
+    double window_size; 
+    double window_start;
+    double window_end;
+
+    // Parse all the command line arguments.  If there are no valid command
+    // line arguments passed, print the usage and exit the application
+    static struct option long_options[] = {
+        {"file_name",  required_argument, 0, 'i'},
+        {"number",     required_argument, 0, 'n'},
+        {"order",      required_argument, 0, 'o'},
+        {"window",     required_argument, 0, 'w'},
+        {"start",      required_argument, 0, 's'},
+        {"end",        required_argument, 0, 'e'},
+        {"help",       no_argument,       0, 'h'},
+        {0, 0, 0, 0}
+    };
+    
+    int option_index = 0;
+    int option_char; 
+    while ((option_char = getopt_long(argc, argv, "i:n:o:w:s:e:h", long_options, &option_index)) != -1) {
+
+        switch(option_char){
+            case 'i': 
+                file_name = optarg;
+                break;
+            case 'n':
+                event_count = atoi(optarg);
+                break;
+            case 'o': 
+                poly_order = atoi(optarg);
+                break;
+            case 'w':
+                window_size = atof(optarg);
+                break;
+            case 's':
+                window_start = atof(optarg);
+                break;
+            case 'e':
+                window_end = atof(optarg);
+                break;
+            case 'h':
+                return EXIT_SUCCESS; 
+            default: 
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (file_name.empty()) { 
+        cerr << "[ EVALUATOR ]: Please specify a file to process." << endl;
+        cerr << "[ EVALUATOR ]: Use --help for usage." << endl;
+        return EXIT_FAILURE;
+    }
+
+    TFile* file = new TFile(file_name.c_str());
+
+    RootFileReader* reader = new RootFileReader(); 
+    reader->parseFile(file);
+
+    for (auto& histo : reader->get1DHistograms()) { 
+        std::cout << "Histogram: " << histo->GetName() << std::endl;
+    }
+}
