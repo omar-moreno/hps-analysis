8689b29599afad6520ab5bc169c7323c138af185
Omar Moreno <omoreno1@ucsc.edu>
2016-01-20 15:33:43 -0800

    Add support for processing multiple files.  If multiple files are incuded, the variables from each of the files are overlayed on the same histogram.

--- python/analysis/generate_bump_hunt_plots.py
+++ python/analysis/generate_bump_hunt_plots.py
@@ -16,77 +16,101 @@ def main() :
     matplotlib.rcParams.update({'font.size': 8})
 
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument("-i", "--input", help="ROOT file")
+    parser.add_argument("-l", "--input_list", help="ROOT file")
     args = parser.parse_args()
 
-    if args.input is None : 
-        print 'A ROOT file needs to be specified'
+    if args.input_list is None : 
+        print 'A list of ROOT files needs to be specified'
         sys.exit(2)
 
-    results_rec = rnp.root2array(args.input)
-    results_array = rnp.rec2array(results_rec)
+    file_list = open(args.input_list, 'r')
+    results_array_list = []
 
-    ap_masses = np.unique(results_array[:,0])
-    yield_mean_list = []
-    yield_sigma_list = []
-    yield_pull_list = []
+    yield_mean_array_list = []
+    yield_sigma_array_list = []
+    yield_pull_array_list = []
+
+    for line in file_list : 
+        
+        print "Processing file: " + str(line.strip())
+
+        results_rec = rnp.root2array(line.strip())
+        results_array_list.append(rnp.rec2array(results_rec))
+    
+        yield_mean_array_list.append([])
+        yield_sigma_array_list.append([])
+        yield_pull_array_list.append([])
+
+    ap_masses = np.unique(results_array_list[0][:,0])
+    print "A' masses: " + str(ap_masses)
 
     with PdfPages("results.pdf") as pdf :
 
-        for ap_mass in ap_masses : 
+        for ap_mass in ap_masses :
+
             print "Processing A' mass: " + str(ap_mass)
+
+            fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(15, 5))
+
+            index = 0
+            for results_array in results_array_list : 
             
-            yield_array = results_array[:,1][results_array[:,0] == ap_mass]
-            yield_error_array = results_array[:,2][results_array[:,0] == ap_mass]
+                yield_array = results_array[:,1][results_array[:,0] == ap_mass]
+                yield_error_array = results_array[:,2][results_array[:,0] == ap_mass]
             
-            fig, (ax0, ax1, ax2) = plt.subplots(ncols=3)
+                ax0.hist(yield_array, bins=50, alpha=0.8, histtype="stepfilled", normed=True)
+                ax0.set_xlabel('Background yield', fontsize=8)
+                ax0.set_ylabel('AU', fontsize=8)
+                ax0.set_title("$A'$ mass hypothesis = " + str(ap_mass), fontsize=8)
 
-            ax0.hist(yield_array, bins=100, alpha=0.8, histtype="stepfilled", normed=True)
-            ax0.set_xlabel('Background yield', fontsize=8)
-            ax0.set_ylabel('AU', fontsize=8)
-            ax0.set_title("$A'$ mass hypothesis = " + str(ap_mass), fontsize=8)
+                mu, std = norm.fit(yield_array)
+                yield_mean_array_list[index].append(mu)
+                yield_sigma_array_list[index].append(std)
+                xmin, xmax = ax0.get_xlim()
+                x = np.linspace(xmin, xmax, 100)
+                p = norm.pdf(x, mu, std)
+                ax0.plot(x, p, linewidth=2)
 
-            mu, std = norm.fit(yield_array)
-            yield_mean_list.append(mu)
-            yield_sigma_list.append(std)
-            xmin, xmax = ax0.get_xlim()
-            x = np.linspace(xmin, xmax, 100)
-            p = norm.pdf(x, mu, std)
-            ax0.plot(x, p, linewidth=2)
+                ax1.hist(yield_error_array, bins=50, alpha=0.8, histtype="stepfilled", normed=True)
+                ax1.set_xlabel('Background yield fit error', fontsize=8)
+                ax1.set_ylabel('AU', fontsize=8)
+                ax1.set_title("$A'$ mass hypothesis = " + str(ap_mass), fontsize=8)
 
-            ax1.hist(yield_error_array, bins=100, alpha=0.8, histtype="stepfilled", normed=True)
-            ax1.set_xlabel('Background yield fit error', fontsize=8)
-            ax1.set_ylabel('AU', fontsize=8)
-            ax1.set_title("$A'$ mass hypothesis = " + str(ap_mass), fontsize=8)
+                ax2.hist(yield_array/yield_error_array, bins=50, alpha=0.8, histtype="stepfilled", normed=True)
+                ax2.set_xlabel('Pull', fontsize=8)
+                ax2.set_ylabel('AU', fontsize=8)
+                ax2.set_title("$A'$ mass hypothesis = " + str(ap_mass),fontsize=8)
 
-            ax2.hist(yield_array/yield_error_array, bins=100, alpha=0.8, histtype="stepfilled", normed=True)
-            ax2.set_xlabel('Pull', fontsize=8)
-            ax2.set_ylabel('AU', fontsize=8)
-            ax2.set_title("$A'$ mass hypothesis = " + str(ap_mass),fontsize=8)
 
-            mu, std = norm.fit(yield_array/yield_error_array)
-            yield_pull_list.append(mu)
-            xmin, xmax = ax2.get_xlim()
-            x = np.linspace(xmin, xmax, 100)
-            p = norm.pdf(x, mu, std)
-            ax2.plot(x, p, linewidth=2)
+                mu, std = norm.fit(yield_array/yield_error_array)
+                yield_pull_array_list[index].append(mu)
+                xmin, xmax = ax2.get_xlim()
+                x = np.linspace(xmin, xmax, 100)
+                p = norm.pdf(x, mu, std)
+                ax2.plot(x, p, linewidth=2)
+
+                index += 1
 
             pdf.savefig()
             plt.close()
-        
-        yield_mean_array = np.array(yield_mean_list)
-        yield_sigma_array = np.array(yield_sigma_list)
-        yield_pull_array = np.array(yield_pull_list)
 
         fig, (ax0, ax1) = plt.subplots(ncols=2)
-        ax0.plot(ap_masses, yield_mean_array, 'o-')
-        ax0.set_xlabel("$A'$ mass hypothesis")
-        ax0.set_ylabel("Background yield")
-        ax0.fill_between(ap_masses, yield_mean_array - yield_sigma_array, yield_mean_array + yield_sigma_array)
+            
+        for index in range(0, len(yield_mean_array_list)) : 
 
-        ax1.plot(ap_masses, yield_pull_array, 'o-')
-        ax1.set_xlabel("$A'$ mass hypothesis")
-        ax1.set_ylabel("Background yield pull")
+            yield_mean_array = np.array(yield_mean_array_list[index])
+            yield_sigma_array = np.array(yield_sigma_array_list[index])
+            yield_pull_array = np.array(yield_pull_array_list[index])
+
+            ax0.plot(ap_masses, yield_mean_array, 'o-')
+            ax0.set_xlabel("$A'$ mass hypothesis")
+            ax0.set_ylabel("Background yield")
+            ax0.fill_between(ap_masses, yield_mean_array - yield_sigma_array, yield_mean_array + yield_sigma_array)
+
+            ax1.plot(ap_masses, yield_pull_array, 'o-')
+            ax1.set_xlabel("$A'$ mass hypothesis")
+            ax1.set_ylabel("Background yield pull")
+            #ax1.set_ylim([-10, 10])
 
         pdf.savefig()
         plt.close()
