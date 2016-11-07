634e7879f89764ffa4a126081b2b5ebb22341dc3
Omar Moreno <email@omarmoreno.net>
2016-11-07 10:38:30 -0800

    Add selection efficiency and sig/sqrt(sig+bkg) plots.

--- python/analysis/TridentAnalysis.py
+++ python/analysis/TridentAnalysis.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import math
 import matplotlib
 import matplotlib.pyplot as plt
@@ -22,6 +24,54 @@ class TridentAnalysis :
 
         # Enable the use of LaTeX in titles
         plt.rc('text', usetex=True)
+
+    def plt_sel_eff(self, ax, signal, min, max, step, invert) :
+
+        cut = min
+        sel_eff = []
+        cuts    = []
+
+        while cut <= max : 
+            if invert : sig_integrated = len(signal[signal > cut])
+            else : sig_integrated = len(signal[signal < cut])
+            sel_eff.append((sig_integrated/len(signal))*100)
+            cuts.append(cut)
+            cut += step
+        
+        major_ticks = np.arange(0, 101, 10)                                              
+        minor_ticks = np.arange(0, 101, 5)  
+
+        ax.set_yticks(major_ticks)                                                       
+        ax.set_yticks(minor_ticks, minor=True)  
+
+        ax.grid(which='minor', alpha=0.2)                                                
+        ax.grid(which='major', alpha=0.5)        
+        
+        ax.errorbar(cuts, sel_eff, marker='None', linestyle='-')
+        ax.set_ylabel("Selection Efficiency (\%)")
+
+    def plt_sig_bkg(self, ax, signal, bkg, min, max, step, invert) :
+
+        cut = min
+        sig_sqrt_sig_bkg = []
+        cuts    = []
+
+        while cut <= max : 
+            if invert : 
+                sig_integrated = len(signal[signal > cut])
+                bkg_integrated = len(bkg[bkg > cut])
+            else : 
+                sig_integrated = len(signal[signal < cut])
+                bkg_integrated = len(bkg[bkg < cut])
+            sig_sqrt_sig_bkg.append(sig_integrated/math.sqrt(sig_integrated + bkg_integrated))
+            cuts.append(cut)
+            cut += step
+        
+        ax.errorbar(cuts, sig_sqrt_sig_bkg, marker='None', linestyle='-')
+        ax.set_ylabel("$S/\sqrt(S+B)$")
+
+        max_index = np.argmax(sig_sqrt_sig_bkg)
+        print "Signal optimized at: " + str(cuts[max_index])
 
     def load_file(self, root_file) : 
 
@@ -78,16 +128,41 @@ class TridentAnalysis :
             plt.hist(electron_cluster_time_real, bins, histtype="step")
             pdf.savefig()
             plt.close()
+           
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
 
-            #rejection_efficiency = []
-            #for index in xrange(0, 60) : 
-            #    rejection_efficiency.append((len(
+            self.plt_sel_eff(ax, electron_cluster_time_real, 20, 60, 0.0625, False)
+            self.plt_sel_eff(ax, electron_cluster_time_real, 20, 60, 0.0625, True)
+            ax.set_xlabel("Electron cluster time (ns)")
+            pdf.savefig()
+            plt.close()
 
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
+            self.plt_sig_bkg(ax, electron_cluster_time_real, electron_cluster_time_acc, 20, 60, 0.0625, False)
+            self.plt_sig_bkg(ax, electron_cluster_time_real, electron_cluster_time_acc, 20, 60, 0.0625, True)
+            ax.set_xlabel("Electron cluster time (ns)")
+            pdf.savefig()
+            plt.close()
 
             bins = np.linspace(0, 180, 361)
             plt.hist(self.positron_cluster_time, bins, histtype="step")
             plt.hist(positron_cluster_time_acc, bins, histtype="step")
             plt.hist(positron_cluster_time_real, bins, histtype="step")
+            pdf.savefig()
+            plt.close()
+
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
+            
+            self.plt_sel_eff(ax, positron_cluster_time_real, 20, 60, 0.0625, False)
+            self.plt_sel_eff(ax, positron_cluster_time_real, 20, 60, 0.0625, True)
+            ax.set_xlabel("Positron cluster time (ns)")
+            pdf.savefig()
+            plt.close()
+
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
+            self.plt_sig_bkg(ax, positron_cluster_time_real, positron_cluster_time_acc, 20, 60, 0.0625, False)
+            self.plt_sig_bkg(ax, positron_cluster_time_real, positron_cluster_time_acc, 20, 60, 0.0625, True)
+            ax.set_xlabel("Positron cluster time (ns)")
             pdf.savefig()
             plt.close()
 
@@ -97,14 +172,41 @@ class TridentAnalysis :
             plt.hist(electron_chi2_real, bins, histtype="step")
             pdf.savefig()
             plt.close()
+
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
             
+            self.plt_sel_eff(ax, electron_chi2_real, 1, 100, .5, False)
+            ax.set_xlabel("Electron $\chi^2$")
+            pdf.savefig()
+            plt.close()
+
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
+            self.plt_sig_bkg(ax, electron_chi2_real, electron_chi2_acc, 1, 100, .5, False)
+            ax.set_xlabel("Electron $\chi^2$")
+            pdf.savefig()
+            plt.close()
+
+
             bins = np.linspace(0, 1.5, 151)
             plt.hist(self.electron_p, bins, histtype="step")
             plt.hist(electron_p_acc, bins, histtype="step")
             plt.hist(electron_p_real, bins, histtype="step")
             pdf.savefig()
             plt.close()
+
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
             
+            self.plt_sel_eff(ax, electron_p_real, .1, 1.4, .05, False)
+            ax.set_xlabel("$p(e^-)$")
+            pdf.savefig()
+            plt.close()
+
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7.5, 7.5))   
+            self.plt_sig_bkg(ax, electron_p_real, electron_p_acc, .1, 1.4, .05, False)
+            ax.set_xlabel("$p(e^-)$")
+            pdf.savefig()
+            plt.close()
+
             bins = np.linspace(0, 50, 51)
             plt.hist(self.v_chi2, bins, histtype="step")
             plt.hist(v_chi2_acc, bins, histtype="step")
